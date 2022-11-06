a942b4d56cfc3ebeb86080e749e985190eb3900a
Mayur Deshmukh <deshmukhmayur@outlook.com>
2022-11-06 06:50:44 +0000

    update: refactor _includes and add seo meta tags

--- src/_data/site.js
+++
@@ -1,8 +0,0 @@
-module.exports.title = 'Mayur Deshmukh'
-module.exports.logo = '/img/logo.svg'
-module.exports.socials = [
-  { name: 'youtube', url: 'https://www.youtube.com/channel/UCf3NC72EEPA2_VB0JX8A1Dg' },
-  { name: 'instagram', url: 'https://instagra.com/deshmukhmayur' },
-  { name: 'twitter', url: 'https://twitter.com/deshmukhmayur_' },
-  { name: 'linkedin', url: 'https://www.linkedin.com/in/deshmukhmayur204/' },
-]
