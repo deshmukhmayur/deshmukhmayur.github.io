842c8c2584df6fa8a8dca8f9e8d0f91df0538920
Mayur Deshmukh <deshmukhmayur@outlook.com>
2022-11-06 06:50:55 +0000

    add: html minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,25 @@
+const htmlmin = require('html-minifier')
+const env = require('./src/_data/env')
+
 module.exports = (config) => {
   config.addPassthroughCopy('src/img')
   config.addPassthroughCopy('src/styles')
   config.addPassthroughCopy('src/scripts')
 
-  config.addWatchTarget('src/**/*.html');
+  config.addWatchTarget('src/**/*.html')
+
+  if (env.isProduction) {
+    config.addTransform('htmlmin', (content, outputPath) => {
+      if (outputPath && outputPath.endsWith('.html')) {
+        return htmlmin.minify(content, {
+          useShortDoctype: true,
+          removeComments: true,
+          collapseWhitespace: true,
+        })
+      }
+      return content
+    })
+  }
 
   return {
     htmlTemplateEngine: 'njk',
